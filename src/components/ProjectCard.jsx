419d6b459bc569bb27c42e45a58d6ae1f9950dac
Zachary Baca <zachary.baca@msn.com>
2026-01-23 12:54:15 -0600

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,29 @@
+// src/components/ProjectCard.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ProjectCard = ({ project }) => {
+const ProjectCard = ({ data }) => {
+  // HELPER: Grab the first image, or use a placeholder if empty
+  const thumbnail = data.imageUrls && data.imageUrls.length > 0 
+    ? data.imageUrls[0] 
+    : "https://placehold.co/600x400?text=No+Image";
+
   return (
     <div className="cell">
-      <Link to={`/project/${project.id}`}>
-        {/* We use the first image in the array as the thumbnail */}
-        <img
-          className="thumbnail"
-          src={project.image_urls[0]}
-          alt={project.project_name}
-        />
-        <h5 style={{ textAlign: "center", marginTop: "10px" }}>
-          {project.project_name}
-        </h5>
-      </Link>
+      {/* Use the thumbnail variable */}
+      <img src={thumbnail} alt={data.title} className="responsive-image" />
+      
+      <div className="projects-box">
+        <Link to={`/project/${data._id}`}> {/* Assuming MongoDB uses _id */}
+          <h5>{data.title}</h5>
+        </Link>
+        {/* Render tags if they exist */}
+        <div className="tags">
+            {data.tags && data.tags.map((tag, index) => (
+                <span key={index} className="badge">{tag}</span>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
