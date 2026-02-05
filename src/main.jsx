9f1a7906ae6e971943ae1218be970bd5eba678e4
Zachary Baca <zachary.baca@msn.com>
2026-02-05 10:50:08 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// 1. IMPORT BrowserRouter
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext'; 
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 2. WRAP App inside BrowserRouter */}
     <BrowserRouter>
-      <App />
+      {/* 2. Wrap the entire App here */}
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
