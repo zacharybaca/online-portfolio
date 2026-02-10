c7f3eb14e2fd755a5c3a29a00d7ee6247bc834eb
Zachary Baca <zachary.baca@msn.com>
2026-02-10 07:34:10 -0600

    Update BlogPost.jsx

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,11 +1,25 @@
-const BlogPost = () => {
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-    return (
-        <div>
-            <h1>Blog Post</h1>
-        </div>
-    )
-}
+const BlogPost = (props) => {
+  return (
+    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">{props.title || ''}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+            {props.content || ''}
+        </p>
+        <p>
+            {`Created At: ${props.timestamps}`}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-
-export default BlogPost
+export default BlogPost;
